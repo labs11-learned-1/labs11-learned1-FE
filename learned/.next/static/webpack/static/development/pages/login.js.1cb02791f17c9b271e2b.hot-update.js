d5c0c5fc4df31887f696194b5e757c7aa1067bfd
Chiboba <briangfelix@gmail.com>
2019-03-20 15:27:51 -0700

    Added the login and register page, functioning with hooks

---
+++ learned/.next/static/webpack/static/development/pages/login.js.1cb02791f17c9b271e2b.hot-update.js
@@ -0,0 +1,51 @@
+webpackHotUpdate("static\\development\\pages\\login.js",{
+
+/***/ "./components/Authentication/Authentication.js":
+/*!*****************************************************!*\
+  !*** ./components/Authentication/Authentication.js ***!
+  \*****************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _jsxFileName = "C:\\Users\\brian\\Desktop\\git\\Projects\\labs11-learned1-FE\\learned\\components\\Authentication\\Authentication.js";
+
+
+var Authentication = function Authentication(props) {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "verifyPage",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 5
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("input", {
+    onChange: function onChange(ev) {
+      props.setUsername(ev.target.value);
+    },
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 6
+    },
+    __self: this
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("input", {
+    onChange: function onChange(ev) {
+      props.setPassword(ev.target.value);
+    },
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 10
+    },
+    __self: this
+  }));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Authentication);
+
+/***/ })
+
+})
+//# sourceMappingURL=login.js.1cb02791f17c9b271e2b.hot-update.js.map
