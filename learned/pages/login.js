dbbe3dd99c4f2793e9c360aadb6d86ae6170e378
James <ruiz.james12@yahoo.com>
2019-03-21 13:49:16 -0700

    Google Auth Implemented

--- learned/pages/login.js
+++ learned/pages/login.js
@@ -1,12 +1,28 @@
 import React, { useState } from 'react';
 import Authentication from '../components/Authentication/Authentication'
+import * as firebase from 'firebase';
+
+var provider = new firebase.auth.GoogleAuthProvider();
 
 const submitInfo = (email, password) => {
     console.log(email, password)
 }
 
 const handleGoogle = () => {
-    console.log("Google login")
+  firebase.auth().signInWithPopup(provider).then((result) => {
+    //var token = result.credential.accessToken;
+    console.log('Logging in')
+  }).catch((e) => {
+    console.log('Error logging in')
+  });
+}
+
+const handleSignOut = () => {
+  firebase.auth().signOut().then(() => {
+    alert('Signed out')
+  }).catch((e) => {
+    alert('Error signing out')
+  });
 }
 
 const Login = () => {
@@ -16,7 +32,7 @@ const Login = () => {
 
     return (
       <div className="loginPage">
-        <Authentication type="login" handleGoogle={handleGoogle} submitInfo={function() { submitInfo(email, password)}} setEmail={setEmail} setPassword={setPassword}/>
+        <Authentication type="login" handleGoogle={handleGoogle} handleSignOut={handleSignOut} submitInfo={function() { submitInfo(email, password)}} setEmail={setEmail} setPassword={setPassword}/>
       </div>
     )
   }
