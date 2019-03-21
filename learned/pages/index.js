83880b1bb7366682f06512a52139e0c957494644
Hunter315 <hunter.mckay.smith@gmail.com>
2019-03-21 16:17:06 -0600

    comment server request in index.js, delete appjs and indexjs in src

--- learned/pages/index.js
+++ learned/pages/index.js
@@ -5,17 +5,19 @@ import {StoreProvider} from '../components/store'; //importing the store provide
 import React from 'react';
 //accessing collection 'user' from firestore then calling .doc to access the document or 'id' field of each user
   //then calling .get to retrieve the data of that user
-        database.collection('user').doc('88kXCz9j4hxYGdzz8TeP').get()
-        .then(function(doc) {
-          if (doc.exists) {
-              console.log("Document data:", doc.data());
-          } else {
-              console.log("No such document!");
-          }
-        })
-        .catch(function(error) {
-          console.log("Error getting document:", error);
-        });
+
+
+        // database.collection('user').doc('88kXCz9j4hxYGdzz8TeP').get()
+        // .then(function(doc) {
+        //   if (doc.exists) {
+        //       console.log("Document data:", doc.data());
+        //   } else {
+        //       console.log("No such document!");
+        //   }
+        // })
+        // .catch(function(error) {
+        //   console.log("Error getting document:", error);
+        // });
 
         //makes a request to get all 500 users and returns an array of objects
 
