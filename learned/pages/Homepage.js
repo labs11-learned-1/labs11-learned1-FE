3f507821ac6cdd9e9cfef24dc3c5a004bda477c8
Drew <drewsup123c@gmail.com>
2019-03-21 13:35:39 -0600

    all routes set up for next

---
+++ learned/pages/Homepage.js
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+//  https://balsamiq.cloud/snv27r3/pqwdr68/r0330
+export default function Homepage() {
+    return (
+        <div>
+            <h1>Homepage of the app</h1>
+            <Link href="/learning-lab"><a>Learning Lab</a></Link>
+            <Link href="/browse" ><a>Browse</a></Link>
+            <Link href="/community" > Community </Link>
+        </div>
+    )
+}
