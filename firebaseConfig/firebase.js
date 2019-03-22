d90f38863d7f3dbb4cbec4b9711f724fe545a138
Hunter315 <hunter.mckay.smith@gmail.com>
2019-03-21 18:07:37 -0600

    Move everything out of learned

---
+++ firebaseConfig/firebase.js
@@ -0,0 +1,17 @@
+// import firebase from 'firebase';
+
+// var config = {
+//     apiKey: "AIzaSyC9SaOiNqXgH1cwwvCu6-14d7y8SFd2eB0",
+//     authDomain: "learned-11.firebaseapp.com",
+//     databaseURL: "https://learned-11.firebaseio.com",
+//     projectId: "learned-11",
+//     storageBucket: "learned-11.appspot.com",
+//     messagingSenderId: "114319083997"
+//   };
+
+//   firebase.initializeApp(config);
+
+//   const database = firebase.firestore()
+
+//   export { config, firebase, database as default };
+//   // export default config;
