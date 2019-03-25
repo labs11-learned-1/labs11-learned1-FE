741d007ef3d93bbe4558699560c7b98e07f00cbd
Drew <drewsup123c@gmail.com>
2019-03-25 17:19:39 -0600

    fixed link in learning-lab.js

--- pages/learning-lab.js
+++ pages/learning-lab.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import Link from "next/link";
 import SearchCourses from '../components/searchCourses';
 
 function LandingPage(){
     return(
         <div>
+            {/* These links will be placed into a navbar component at some point */}
+            <Link href="/Homepage">
+                <a>Home</a>
+            </Link>
+            <Link href="/learning-lab">
+                <a>Learning Lab</a>
+            </Link>
+            <Link href="/browse">
+                <a>Browse</a>
+            </Link>
+            <Link href="/community">
+                <a>Community</a>
+            </Link>
+
             <h1>Current Courses</h1>
             <div className="thisIsWhereCoursesCardsWillGo">
-            
+
             </div>
             <h1>My List</h1>
             <div className="IDKWTFThisIs">
