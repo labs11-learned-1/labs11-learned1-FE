82ba86136a81fd85d23dd5386c920c33bec660ff
Hunter315 <hunter.mckay.smith@gmail.com>
2019-03-25 16:11:10 -0600

    clean up directories, only one store, full debug

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,9 @@
 import React, {useContext} from "react";
-
 import LandingPage from "./LandingPage";
-//importing the store provider from store.js
-import { StoreConsumer, StoreContext } from '../components/StoreProvider'
-import LandingNav from "../components/landingNav";
-// import firebase from '../firebaseConfig/firebase.js'
-
-
 
 export default class Home extends React.Component {
   //first instance of the database loaded into the app
-
   // let firebase = await loadDB();
-  
   render() {
     return (
       <LandingPage />
@@ -20,16 +11,3 @@ export default class Home extends React.Component {
     );
   }
 }
-
-//makes a request to get all 500 users and returns an array of objects
-
-/* database.collection('user').get().then(function(querySnapshot) {
-          const users = []
-          querySnapshot.forEach((doc) => {
-            users.push({
-              id: doc.id,
-              ...doc.data()
-            })
-          })
-          console.log(users)
-      }); */
