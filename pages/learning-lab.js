77a117a4a4e8257c10468a51bed4b0b399ecb108
Drew <drewsup123c@gmail.com>
2019-03-25 17:08:51 -0600

    added learning-lab page with basic boilerplate

---
+++ pages/learning-lab.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import SearchCourses from '../components/searchCourses';
+
+function LandingPage(){
+    return(
+        <div>
+            <h1>Current Courses</h1>
+            <div className="thisIsWhereCoursesCardsWillGo">
+            
+            </div>
+            <h1>My List</h1>
+            <div className="IDKWTFThisIs">
+            
+            </div>
+        </div>
+    );
+}
+
+export default LandingPage;
