bc833c13a19ae37316fdf0d5da65ad21bf8b766c
AM_Myrick <amyrick.820@gmail.com>
2019-03-25 15:11:39 -0400

    testing that hidden api keys work in deployment

--- firebaseConfig/firebase.js
+++ firebaseConfig/firebase.js
@@ -4,12 +4,12 @@ import '@firebase/firestore'
 export function loadDB() {
 try{
   const config = {
-    apiKey: process.env.firebase_key || "@FIREBASE_KEY",
-    authDomain: process.env.auth_domain || "@AUTH_DOMAIN",
-    databaseURL: process.env.database_url || "@DATABASE_URL",
-    projectId: process.env.project_id || "@PROJECT_ID",
-    storageBucket: process.env.storage_bucket || "@STORAGE_BUCKET",
-    messagingSenderId: process.env.messaging_sender_id || "@MESSAGING_SENDER_ID"
+    apiKey: process.env.FIREBASE_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID
   };
  
   firebase.initializeApp(config);
