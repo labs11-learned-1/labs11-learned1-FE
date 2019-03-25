148be82e569021b7d966d9f61ad9dc1411a55750
Drew <drewsup123c@gmail.com>
2019-03-25 14:22:56 -0600

    fixed merge conflict

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,5 @@
 import App, { Container } from 'next/app'
 /* First we import our provider */
-<<<<<<< HEAD
-import StoreProvider from '../components/StoreProvider'
-
-class MyApp extends App {
-  render () {
-    const { Component, pageProps } = this.props
-    return (
-      <Container>
-        {/* Then we wrap our components with the provider */}
-        <StoreProvider>
-          <Component/>
-        </StoreProvider>
-      </Container>
-    )
-  }
-=======
 import StoreProvider from '../components/store'
 
 class MyApp extends App {
@@ -30,7 +14,6 @@ class MyApp extends App {
             </Container>
         )
     }
->>>>>>> bd56c4975a9cead47f5ebc12f14372483507263d
 }
 
 export default MyApp
