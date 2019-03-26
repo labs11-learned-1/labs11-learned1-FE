7824404c3fecdc06be24e9367bf982c9c0437faa
Drew <drewsup123c@gmail.com>
2019-03-26 01:08:15 -0600

    learning-lab scoffolding complete, or as close as I can get

--- pages/learning-lab.js
+++ pages/learning-lab.js
@@ -1,25 +1,75 @@
 import React from 'react';
-import Link from "next/link";
-// Bringing in SearchCourses for future reference
-import SearchCourses from '../components/searchCourses';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
 import Nav from '../components/Navigation/Nav'
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
-function LandingPage(){
-    return(
+export default class LearningLab extends React.Component {
+    state = {
+        open: false,
+    };
+
+    handleClickOpen = () => {
+        this.setState({ open: true });
+    };
+
+    handleClose = () => {
+        this.setState({ open: false });
+    };
+
+    render() {
+        return (
         <div>
-            {/* These links will be placed into a navbar component at some point */}
-            
             <Nav />
+
             <h1>Current Courses</h1>
             <div className="thisIsWhereCoursesCardsWillGo">
-
+            {/* This is where user courses will show up */}
             </div>
             <h1>My List</h1>
             <div className="IDKWTFThisIs">
-            
+            {/* I still have no Idea what this is */}
             </div>
-        </div>
-    );
-}
+            <Fab color="primary" aria-label="Add" onClick={this.handleClickOpen}>
+                <AddIcon />
+            </Fab>
 
-export default LandingPage;
+            {/* Modul starts here */}
+            <Dialog
+            open={this.state.open}
+            onClose={this.handleClose}
+            aria-labelledby="form-dialog-title"
+            >
+                <DialogTitle id="form-dialog-title">Enter Link to Blog/Course</DialogTitle>
+
+                <DialogContent>
+                    <TextField
+                    autoFocus
+                    margin="dense"
+                    id="name"
+                    label="Link"
+                    fullWidth
+                    />
+                </DialogContent>
+
+                <DialogActions>
+                    <Button onClick={this.handleClose} color="primary">
+                    Cancel
+                    </Button>
+                    {/* Change this to handle submit */}
+                    <Button onClick={this.handleClose} color="primary">
+                    Add
+                    </Button>
+                </DialogActions>
+
+            </Dialog>
+        </div>
+        );
+    }
+}
