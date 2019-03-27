2113ee8efe96672964ef260c687fdc9ac80f44a7
AM_Myrick <amyrick.820@gmail.com>
2019-03-27 16:09:35 -0400

    attempts to fix deployment err with webpack plugin

--- next.config.js
+++ next.config.js
@@ -1,29 +1,62 @@
+const webpack = require('webpack');
+require('dotenv').config();
 
-    const webpack = require('webpack');
-    require('dotenv').config();
+const withAssetRelocator = (nextConfig = {}) => {
+  return Object.assign({}, nextConfig, {
+    webpack(config, options) {
+      const { isServer } = options
 
-    module.exports = {
-      webpack: config => {
-        const env = Object.keys(process.env).reduce((acc, curr) => {
-          acc[`process.env.${curr}`] = JSON.stringify(process.env[curr]);
-          return acc;
-        }, {});
+      if (isServer) {
+        config.node = Object.assign({}, config.node, {
+          __dirname: false,
+          __filename: false,
+        })
 
-        config.plugins.push(new webpack.DefinePlugin(env));
-
-        return config;
-      },
-      
-      target: 'serverless',
-      env: {
-        "FIREBASE_KEY": process.env.FIREBASE_KEY,
-        "AUTH_DOMAIN": process.env.AUTH_DOMAIN,
-        "DATABASE_URL": process.env.DATABASE_URL,
-        "PROJECT_ID": process.env.PROJECT_ID,
-        "STORAGE_BUCKET": process.env.STORAGE_BUCKET,
-        "MESSAGING_SENDER_ID": process.env.MESSAGING_SENDER_ID,
-        "UDEMY_CLIENT_ID": process.env.UDEMY_CLIENT_ID,
-        "UDEMY_CLIENT_SECRET": process.env.UDEMY_CLIENT_SECRET
+        config.module.rules.unshift({
+          test: /\.(m?js|node)$/,
+          parser: { amd: false },
+          use: {
+            loader: '@zeit/webpack-asset-relocator-loader',
+            options: {
+              outputAssetBase: 'assets',
+              existingAssetNames: [],
+              wrapperCompatibility: true,
+              escapeNonAnalyzableRequires: true,
+            },
+          },
+        })
       }
-      
-    };
+
+      if (typeof nextConfig.webpack === 'function') {
+        return nextConfig.webpack(config, options)
+      }
+      return config
+    },
+  })
+}
+
+module.exports = {
+  webpack: config => {
+    const env = Object.keys(process.env).reduce((acc, curr) => {
+      acc[`process.env.${curr}`] = JSON.stringify(process.env[curr]);
+      return acc;
+    }, {});
+
+    config.plugins.push(new webpack.DefinePlugin(env));
+
+    return config;
+  },
+  
+  target: 'serverless',
+  env: {
+    "FIREBASE_KEY": process.env.FIREBASE_KEY,
+    "AUTH_DOMAIN": process.env.AUTH_DOMAIN,
+    "DATABASE_URL": process.env.DATABASE_URL,
+    "PROJECT_ID": process.env.PROJECT_ID,
+    "STORAGE_BUCKET": process.env.STORAGE_BUCKET,
+    "MESSAGING_SENDER_ID": process.env.MESSAGING_SENDER_ID,
+    "UDEMY_CLIENT_ID": process.env.UDEMY_CLIENT_ID,
+    "UDEMY_CLIENT_SECRET": process.env.UDEMY_CLIENT_SECRET
+  }
+  
+};
