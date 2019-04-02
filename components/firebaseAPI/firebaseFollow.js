0c14ea1cb39edc0acb3a1fe1020d9fc8fbb1526e
Hunter315 <hunter.mckay.smith@gmail.com>
2019-04-01 19:39:38 -0600

    followerCount and followingCount set on user

--- components/firebaseAPI/firebaseFollow.js
+++ components/firebaseAPI/firebaseFollow.js
@@ -1,83 +1,104 @@
 import * as firebase from "firebase";
 import { loadDB } from "../../firebaseConfig/firebase";
 
+// ====== User1 follows User2
+export const getPostsOfFollowing = async () => {
+  let result = await loadDB();
+  let db = result.firestore();
+  let userRef = db.collection("user");
+  
+  let postsRef = db.collection("posts");
 
-
-// ====== User1 follows User2 
- export const getPostsOfFollowing = async () => {
-    let result = await loadDB();
-    let db = result.firestore();
-    let userRef = db.collection('user');
-    let followRef = db.collection('relationships');//might not need this
-    let postsRef = db.collection('posts');
-
-
-    //get array of who user follows
-    const followingArray = await userRef.doc("450").get(firebase.firestore.FieldPath("following")).then(docSnapshot => {
-        console.log(docSnapshot.data().following)
-        return(docSnapshot.data().following)
-    }).catch(err => {
-        console.log(err)
+  //get array of who user follows
+  const followingArray = await userRef
+    .doc("450")
+    .get(firebase.firestore.FieldPath("following"))
+    .then(docSnapshot => {
+      console.log(docSnapshot.data().following);
+      return docSnapshot.data().following;
     })
-    
-    //get posts of each user in array above
-    let data =  await followingArray.forEach(user => postsRef.where("userId", "==", user).limit(1).get().then(querySnapshot => {
-        querySnapshot.forEach(post => console.log("POST:   ", post.data()))
-    }).catch(err => {
-        console.log(err)
-    })
-    
-    );
+    .catch(err => {
+      console.log(err);
+    });
 
-    console.log("RESULT: ", data)
+  //get posts of each user in array above
+  let data = await followingArray.forEach(user =>
+    postsRef
+      .where("userId", "==", user)
+      .get()
+      .then(querySnapshot => {
+        querySnapshot.forEach(post => console.log("POST:   ", post.data()));
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  );
 
-}
+  console.log("RESULT: ", data);
+};
 
 // ---------- FOLLOW OTHERS -------- //
 
 //need to update followCount and followersCount
-export const followOthers = async () => { // async (myUserId, theirId)
-    let result = await loadDB();
-    let db = result.firestore();
+export const followOthers = async () => {
+  // async (myUserId, theirId)
+  let result = await loadDB();
+  let db = result.firestore();
 
-    let userRef = db.collection('user');
-     userRef.doc('450').get().then(docSnapshot => {
-//check if user is already following
-        if(docSnapshot.data().following.includes("454")){
-            //====================unfollow section==================
-            userRef.doc('450').update({ following: firebase.firestore.FieldValue.arrayRemove("454")}) //if user "454" is in 450's following then remove
-            .then(()=> { 
-                userRef.doc('454').update({followers: firebase.firestore.FieldValue.arrayRemove('450')}) //then remove "450" from 454's followers
-                .then(()=> {
-                    console.log("success unfollowing")
-                }).catch(err => console.log('Error updating other users followers'))
-            })
-            .catch(err => console.log('Error removing from following'))
-        } else { 
-            //=================follow section=====================
-            //if user 450 is not following user 454 then adding 454 to following array
-            userRef.doc('450').update({following: firebase.firestore.FieldValue.arrayUnion('454')})// .doc("myUserId")  .arrayUnion("theirId")
-        .then (() => {
-        // then update their followers list with my id
-            userRef.doc('454').update({followers: firebase.firestore.FieldValue.arrayUnion('450')}) // .doc("theirId")  .arrayUnion("myUserId")
-            .then(() => {console.log("Success update follows")})
-            .catch(err => console.log('ERROR updating other users followers')) 
-
-        }).catch(err => console.log('ERROR FOLLOWING USER')) 
-        }
-     })
-    //check if user already follows 
-    
-
-    // // update my following list with their id
-    // userRef.doc('450').update({following: firebase.firestore.FieldValue.arrayUnion('454')})// .doc("myUserId")  .arrayUnion("theirId")
-    // .then (() => {
-    // // then update their followers list with my id
-    //     userRef.doc('454').update({followers: firebase.firestore.FieldValue.arrayUnion('450')}) // .doc("theirId")  .arrayUnion("myUserId")
-    //     .then(() => {console.log("Success update follows")})
-    //     .catch(err => console.log('ERROR updating other users followers')) 
-
-    // }).catch(err => console.log('ERROR FOLLOWING USER')) 
-
-
-}
+  let userRef = db.collection("user");
+  userRef
+    .doc("450")
+    .get()
+    .then(docSnapshot => {
+      //check if user is already following
+      if (docSnapshot.data().following.includes("454")) {
+        //====================unfollow section==================
+        userRef
+          .doc("450")
+          .update({
+            followingCount: firebase.firestore.FieldValue.increment(-1),
+            following: firebase.firestore.FieldValue.arrayRemove("454")
+          }) //if user "454" is in 450's following then remove
+          .then(() => {
+            userRef
+              .doc("454")
+              .update({
+                followerCount: firebase.firestore.FieldValue.increment(-1),
+                followers: firebase.firestore.FieldValue.arrayRemove("450")
+              }) //then remove "450" from 454's followers
+              .then(() => {
+                console.log("success unfollowing");
+              })
+              .catch(err =>
+                console.log("Error updating other users followers", err)
+              );
+          })
+          .catch(err => console.log("Error removing from following"));
+      } else {
+        //=================follow section=====================
+        //if user 450 is not following user 454 then adding 454 to following array
+        userRef
+          .doc("450")
+          .update({
+            followingCount: firebase.firestore.FieldValue.increment(1), // add +1 to following count
+            following: firebase.firestore.FieldValue.arrayUnion("454")
+          }) // .doc("myUserId")  .arrayUnion("theirId")
+          .then(() => {
+            // then update their followers list with my id
+            userRef
+              .doc("454")
+              .update({
+                followerCount: firebase.firestore.FieldValue.increment(1),
+                followers: firebase.firestore.FieldValue.arrayUnion("450")
+              }) // .doc("theirId")  .arrayUnion("myUserId")
+              .then(() => {
+                console.log("Success update follows");
+              })
+              .catch(err =>
+                console.log("ERROR updating other users followers")
+              );
+          })
+          .catch(err => console.log("ERROR FOLLOWING USER"));
+      }
+    });
+};
