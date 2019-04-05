e4ee9a0259b3c876c42cc5d8bedd623906b1a693
Chiboba <briangfelix@gmail.com>
2019-04-04 23:21:32 -0700

    Test

--- components/Algolia/algoliaHandler.js
+++ components/Algolia/algoliaHandler.js
@@ -1,11 +1,11 @@
 import algoliasearch from 'algoliasearch'
-/*
+
 const ALGOLIA_APP_ID = process.env.ALGOLIA_APP_ID
 const ALGOLIA_ADMIN_KEY = process.env.ALGOLIA_ADMIN_KEY
 const ALGOLIA_SEARCH_KEY = process.env.ALGOLIA_SEARCH_KEY
 const ALGOLIA_INDEX_NAME = process.env.ALGOLIA_INDEX_NAME;
 const client = algoliasearch(ALGOLIA_APP_ID, ALGOLIA_ADMIN_KEY);
-
+/*
 export const onPostsCreated = (post) => {
     // Get the note document
 
