eba34ab1caab2b7bcb479b4d4c4c3bb796e3408a
Hunter315 <hunter.mckay.smith@gmail.com>
2019-04-08 14:21:26 -0600

    get rid of webpack config

--- next.config.js
+++ next.config.js
@@ -1,18 +1,7 @@
-// const webpack = require('webpack');
-// require('dotenv').config();
 
 module.exports = {
-  // webpack: config => {
-  //   const env = Object.keys(process.env).reduce((acc, curr) => {
-  //     acc[`process.env.${curr}`] = JSON.stringify(process.env[curr]);
-  //     return acc;
-  //   }, {});
-
-  //   config.plugins.push(new webpack.DefinePlugin(env));
-
-  //   return config;
-  // },
   
+
   target: 'serverless',
   env: {
     "FIREBASE_KEY": process.env.FIREBASE_KEY,
