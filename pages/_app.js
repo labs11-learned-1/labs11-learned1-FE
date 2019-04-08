f4f6b45822f79fc6a36060034f4374ec7c7b066b
Drew <drewsup123c@gmail.com>
2019-04-08 09:55:34 -0600

    added react from react and modal bug is fixed

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import App, { Container } from 'next/app'
 /* First we import our provider */
 import StoreProvider from '../components/store'
+import React from 'react';
 
 class MyApp extends App {
     render () {
